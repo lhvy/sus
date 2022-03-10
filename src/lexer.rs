907fd444869186377b2790da0ebce846af409f47
Lucas <me@lhvy.dev>
2022-03-10 15:18:45 +1100

    Add lexer

---
+++ src/lexer.rs
@@ -0,0 +1,59 @@
+use logos::Logos;
+
+#[derive(Logos)]
+enum TokenKind {
+    #[token("SUS")]
+    Sus,
+
+    #[token("VENTED")]
+    Vented,
+
+    #[token("ELECTRICAL")]
+    Electrical,
+
+    #[regex("WHO\\??")]
+    Who,
+
+    #[token("WHERE\\??")]
+    Where,
+
+    #[token("RED")]
+    Red,
+
+    #[token("BLUE")]
+    Blue,
+
+    #[token("PURPLE")]
+    Purple,
+
+    #[token("GREEN")]
+    Green,
+
+    #[token("YELLOW")]
+    Yellow,
+
+    #[token("CYAN")]
+    Cyan,
+
+    #[token("BLACK")]
+    Black,
+
+    #[token("WHITE")]
+    White,
+
+    #[token("BROWN")]
+    Brown,
+
+    #[token("LIME")]
+    Lime,
+
+    #[token("PINK")]
+    Pink,
+
+    #[token("ORANGE")]
+    Orange,
+
+    #[regex("[ \n\t]+", logos::skip)]
+    #[error]
+    Error,
+}
